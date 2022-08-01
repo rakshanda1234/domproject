ebd6e79555c313b31ad20e702a27b360c1a9b634
Rakshanda wankhade <rakshandawankhade95@gmail.com>
2022-08-01 15:39:18 +0530

    Task-7

--- Item_list/dom.js
+++ Item_list/dom.js
@@ -74,7 +74,7 @@ for (var i = 0; i < li.length; i++) {
 // Task - 6 //
 
 // QUERYSELECTOR //
-var header = document.querySelector("#main-header");
+/*var header = document.querySelector("#main-header");
 header.getElementsByClassName.borderBottom = "solid 4px #ccc";
 
 var input = document.querySelector("input");
@@ -112,3 +112,90 @@ for (var i = 0; i < odd.length; i++) {
   even[i].style.backgroundColor = "#ccc";
   secondItem.style.color = "green";
 }
+*/
+
+// Task-7 //
+
+//TRAVERSING THE DOM
+var itemList = document.querySelector("#items");
+
+//parentNode
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor = "#f4f4f4";
+console.log(itemList.parentNode.parentNode);
+parentNode = document.getElementById("items");
+parentNode.innerHTML = "<li>Hello World</li>" + parentNode.innerHTML;
+
+//parentElement
+/*console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor = "#f4f4f4";
+console.log(itemList.parentElement.parentElement.parentElement);
+*/
+
+//childNodes
+//console.log(itemList.childNodes);
+
+/*console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor = "yellow";
+
+*/
+/*firstChild
+console.log(itemList.firstChild);
+
+//firstElementChild
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent = "Hello 1";
+
+*/
+
+/*LastChild
+console.log(itemList.lastChild);
+//lastElementChild
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent = "Hello 4";
+
+*/
+
+/*nextSibling
+console.log(itemList.nextSibling);
+//nextElementSibling
+console.log(itemList.nextElementSibling);
+*/
+
+/*previousSibling
+console.log(itemList.previousSibling);
+
+//previousElementSibling
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.color = "green";
+*/
+
+//createElement
+
+//create a div
+var newDiv = document.createElement("div");
+
+//Add class
+newDiv.className = "hello";
+
+//Add id
+newDiv.id = "hello 1";
+
+//Add attr
+newDiv.setAttribute("title", "hello Div");
+
+//create text node
+var newDivText = document.createTextNode("Hello World");
+
+//Add text to Div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector("header .container");
+var h1 = document.querySelector("header h1");
+
+console.log(newDiv);
+
+newDiv.style.fontSize = "30px";
+
+container.insertBefore(newDiv, h1);

21727f6334fadb4d7531d0511f31e37d94bf2638
Rakshanda wankhade <rakshandawankhade95@gmail.com>
2022-08-01 15:43:36 +0530

    Task-8

--- Item_list/dom.js
+++ Item_list/dom.js
@@ -174,7 +174,7 @@ itemList.previousElementSibling.style.color = "green";
 //createElement
 
 //create a div
-var newDiv = document.createElement("div");
+/*var newDiv = document.createElement("div");
 
 //Add class
 newDiv.className = "hello";
@@ -199,3 +199,65 @@ console.log(newDiv);
 newDiv.style.fontSize = "30px";
 
 container.insertBefore(newDiv, h1);
+*/
+
+//Task - 8
+
+var form = document.getElementById("addForm");
+var itemList = document.getElementById("items");
+var filter = document.getElementById("filter");
+
+//Form submit event
+form.addEventListener("submit", addItem);
+
+//Delete event
+itemList.addEventListener("click", removeItem);
+
+//filter event
+filter.addEventListener("keyup", filterItems);
+
+//Add Item
+function addItem(e) {
+  e.preventDefault();
+
+  // console.log(1);
+
+  //Get input value
+  var newItem = document.getElementById("item").value;
+
+  ///create new li element
+  var li = document.createElement("li");
+
+  //add class
+  li.className = "list-group-item";
+
+  //add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  itemList.appendChild(li);
+
+  // create del button element
+  var deleteBtn = document.createElement("button");
+
+  //Add classes to del button
+  deleteBtn.className = "btn btn-danger btn-sm float-right delete";
+
+  //Append text node
+  deleteBtn.appendChild(document.createTextNode("X"));
+
+  //Append button to li
+  li.appendChild(deleteBtn);
+
+  //Append li to list
+  itemList.appendChild(li);
+}
+
+//Remove item
+function removeItem(e) {
+  if (e.target.classList.contains("delete")) {
+    if (confirm("Are you sure?")) {
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}

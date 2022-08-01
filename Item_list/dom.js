f57f2a29ab6420c25c5606cf2ad5b614211e674a
Rakshanda wankhade <rakshandawankhade95@gmail.com>
2022-08-01 15:56:50 +0530

    Task-9

--- Item_list/dom.js
+++ Item_list/dom.js
@@ -238,14 +238,18 @@ function addItem(e) {
 
   // create del button element
   var deleteBtn = document.createElement("button");
+  var editBtn = document.createElement("button");
 
   //Add classes to del button
+  editBtn.className = "btn btn-warning btn-sm float-right warning";
   deleteBtn.className = "btn btn-danger btn-sm float-right delete";
 
   //Append text node
+  editBtn.appendChild(document.createTextNode("Edit"));
   deleteBtn.appendChild(document.createTextNode("X"));
 
   //Append button to li
+  li.appendChild(editBtn);
   li.appendChild(deleteBtn);
 
   //Append li to list
@@ -260,4 +264,24 @@ function removeItem(e) {
       itemList.removeChild(li);
     }
   }
+}
+
+//Task - 9 //
+function filterItems(e) {
+  //convert text to lowercase
+  var text = e.target.value.toLowerCase();
+
+  //Get lis
+  var items = itemList.getElementsByTagName("li");
+  console.log(items);
+
+  //convert to an array
+  Array.from(items).forEach(function (item) {
+    var itemName = item.firstChild.textContent;
+    if (itemName.toLowerCase().indexOf(text) != -1) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
 }
